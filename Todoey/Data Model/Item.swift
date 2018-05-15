a1601874d003ca438e052d7683aeac2ac956dba9
Vincent Nguyen <vincent.q.nguyen@gmail.com>
2018-05-14 21:47:22 -0400

    convert to mvc design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Vincent Nguyen on 5/14/18.
+//  Copyright © 2018 Vincent Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+    
+}
