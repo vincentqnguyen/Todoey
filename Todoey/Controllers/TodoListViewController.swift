75808a9b74257248dc476dbfc28c72062d6acdf8
Vincent Nguyen <vincent.q.nguyen@gmail.com>
2018-05-19 22:47:32 -0400

    line fix, add pod swipe cell kit

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -22,7 +22,6 @@ class TodoListViewController: UITableViewController {
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
